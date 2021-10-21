81f1b65e0ebb77d37c4d6c96b40374246c972203
andro ostoic <androstoic@gmail.com>
2021-10-21 15:10:57 -0300

    feat : router

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require("express");
 const indexRoutes = require("./routes/routes");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use("/", indexRoutes);
 
 app.listen(port, function () {
